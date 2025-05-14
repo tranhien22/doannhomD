e9613584798cc02be4461fd60f94cc788b508c96
Luant <luanluong960@gmail.com>
2025-05-14 14:29:07 +0700

    san pham admin crud

--- shopthoitrang/routes/web.php
+++ shopthoitrang/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\ProductController;
 
 
 /*
@@ -14,6 +15,7 @@ use App\Http\Controllers\Admin\CategoryController;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('dashboard', [CategoryController::class, 'dashboard']);
 
 Route::get('category', [CategoryController::class, 'indexCategory'])->name('category.index');
 Route::get('categorycreate', [CategoryController::class, 'indexcreateCategory'])->name('category.createindex');
@@ -22,3 +24,11 @@ Route::get('categoryupdate', [CategoryController::class, 'indexupdateCategory'])
 Route::post('categoryupdate', [CategoryController::class, 'updateCategory'])->name('category.updateCategory');
 Route::get('categorydelete', [CategoryController::class, 'deleteCategory'])->name('category.deleteCategory');
 
+
+//product
+Route::get('listproduct', [ProductController::class, 'indexProduct'])->name('product.listproduct');
+Route::get('addproduct', [ProductController::class, 'indexAddProduct'])->name('product.indexaddproduct');
+Route::post('addproduct', [ProductController::class, 'addProduct'])->name('product.addproduct');
+Route::get('deleteproduct', [ProductController::class, 'deleteProduct'])->name('product.deleteproduct');
+Route::get('updateproduct', [ProductController::class, 'indexUpdateProduct'])->name('product.indexUpdateproduct');
+Route::post('updateproduct', [ProductController::class, 'updateProduct'])->name('product.updateproduct');
