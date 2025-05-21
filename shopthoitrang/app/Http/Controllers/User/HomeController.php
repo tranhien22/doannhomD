4a8288d9ddc774a6a6cf84a3b9f0ff7d899af298
Luant <luanluong960@gmail.com>
2025-05-21 14:16:33 +0700

    cap nhat logic code

--- shopthoitrang/app/Http/Controllers/User/HomeController.php
+++ shopthoitrang/app/Http/Controllers/User/HomeController.php
@@ -10,46 +10,32 @@ use App\Models\Manufacturer;
 
 class HomeController extends Controller
 {
-
     public function indexHome()
     {
-
-        $product = Product::paginate(6);
-        $get6newproduct = Product::orderBy('created_at', 'desc')->take(6)->get();
+        $product = Product::getHomePageProducts(6);
+        $get6newproduct = Product::getLatestProducts(6);
         $category = Category::all();
-        $manufacturer = Manufacturer::all();
-        $productsWithCategorys = Product::select('products.*', 'categories.name_category')
+        $manufacturer = Manufacturer::getAllManufacturers();
+        $productsWithCategorys = Product::getProductsWithCategories();
+        $productsWithManufacturers = Product::getProductsWithManufacturers();
 
-            ->leftJoin('categories', 'products.id_category', '=', 'categories.id_category')
-            ->get();
-        $productsWithManufacturers = Product::select('products.*', 'manufacturers.name_manufacturer')
-            ->leftJoin('manufacturers', 'products.id_manufacturer', '=', 'manufacturers.id_manufacturer')
-            ->get();
         return view('user.home', [
             'products' => $product,
-
             'get6newproducts' => $get6newproduct,
             'categorys' => $category,
             'manufacturers' => $manufacturer,
             'productsWithCategorys' => $productsWithCategorys,
-
             'productsWithManufacturers' => $productsWithManufacturers
         ]);
     }
 
     public function indexDetailProduct(Request $request)
     {
-        $product_id = $request->get('id');
-        $product = Product::where('id_product', $product_id)->first();
-        $specification = $product->specifications;
-        $specificationArray = explode(';', $specification);
-
-        // Lấy danh sách màu sắc và kích thước từ cột `colors` và `sizes`
-        $colors = explode(',', $product->colors);
-        $sizes = explode(',', $product->sizes);
-
-        // Lấy thông tin hãng sản xuất
-        $manufacturer = Manufacturer::where('id_manufacturer', $product->id_manufacturer)->first();
+        $product = Product::findProductById($request->get('id'));
+        $specificationArray = $product->getSpecificationsArray();
+        $colors = $product->getColorsArray();
+        $sizes = $product->getSizesArray();
+        $manufacturer = Manufacturer::findManufacturerById($product->id_manufacturer);
 
         return view('user.detailproduct', [
             'specifications' => $specificationArray,
@@ -62,14 +48,11 @@ class HomeController extends Controller
 
     public function showProductDetail($id)
     {
-        $product = Product::find($id);
-
-        // Lấy danh sách màu sắc và kích thước từ cột `colors` và `sizes`
-        $colors = explode(',', $product->colors);
-        $sizes = explode(',', $product->sizes);
-        $specifications = explode(';', $product->specifications);
+        $product = Product::getProductDetail($id);
+        $colors = $product->getColorsArray();
+        $sizes = $product->getSizesArray();
+        $specifications = $product->getSpecificationsArray();
 
         return view('user.detailproduct', compact('product', 'colors', 'sizes', 'specifications'));
     }
-
 }
